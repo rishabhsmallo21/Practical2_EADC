85594e9e15fff4bb3fa8a0519eb234c0a5e223c6
rishabh <rishabhsmallo21@gnu.ac.in>
2024-02-06 08:53:17 +0530

    updated

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => {
-    res.send('Hello, this is a code by RISHABH MALOO to test pipeline and this is the NEWEST version and I am currently fed up');
-  });
-  const port = process.env.PORT || 3000;
+app.use(express.static(__dirname + '/public'))
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`API server listening on port ${port}`);
 });
---
+++ public/prac1.js
@@ -0,0 +1,20 @@
+/*const header = document.querySelector('.navbar');
+
+window.onscroll = function() {
+    var top = window.scrollY;
+    if(top >=100) {
+        header.classList.add('navbarDark');
+    }
+    else {
+        header.classList.remove('navbarDark');
+    }
+}
+*/
+
+for(let key in user){
+    alert(key);
+    alert(user[key]);
+}
+let user = {
+    name:"john",age:30,isAdmin:true
+};
